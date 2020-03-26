41c14bbade5fd6982bd83bd6b3bc58764cf49904
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-03-26 14:18:59 +0100

    Ajout à FrontOffice Web.UI de EFCore -Connectionstring dans Startup.cs -AddContext dans Startup.cs

--- JeuxDontOnEstLeHero.Web.UI/Startup.cs
+++ JeuxDontOnEstLeHero.Web.UI/Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using jeudontonestlehero.Core.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace JeuxDontOnEstLeHero.Web.UI
 {
@@ -22,6 +24,9 @@ namespace JeuxDontOnEstLeHero.Web.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = this.Configuration.GetConnectionString("DefaultContext");
+            services.AddDbContext<DefaultContext>(options=>options.UseSqlServer(connectionString));
+
             services.AddControllersWithViews();
         }
 

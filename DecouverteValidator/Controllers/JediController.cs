84e4bfc6027f7e7aa438a9cfacec996cce25249f
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-03-28 11:48:55 +0100

    Decouvertye des Validators

---
+++ DecouverteValidator/Controllers/JediController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DecouverteValidator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DecouverteValidator.Controllers
+{
+    public class JediController : Controller
+    {
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Jedi jedi)
+        {
+            if (ModelState.IsValid)
+            {
+                //Vers la BDD
+            }
+            return View(jedi);
+        }
+    }
+}
---
+++ DecouverteValidator/Models/Jedi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecouverteValidator.Models
+{
+    public class Jedi
+    {
+        [Required]
+        public string Nom { get; set; }
+
+        [Range(1, 300)]
+        public int Size { get; set; }
+
+    }
+}
